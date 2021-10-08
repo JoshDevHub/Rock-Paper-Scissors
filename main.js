4c208390e167d9b101e5992fb0c0a61a5c92408a
jmsmith1018 <jmsmith1018@gmail.com>
2021-10-07 18:46:37 -0600

    Refactor and development of RPS game to work with the DOM so the user has an in-browser UI

--- main.js
+++ main.js
@@ -1,62 +1,48 @@
 const rpsArray = ['rock', 'paper', 'scissors'];
+let computerScore = 0;
+let userScore = 0;
 
-const computerPlay =() => rpsArray[Math.floor(Math.random() * 3)];
+const computerPlay = () => rpsArray[Math.floor(Math.random() * 3)];
+
+const playRound = (userInput, computerInput) => {
+  const resultArray = [userInput, computerInput];
+
+  const userRPSIndex = rpsArray.indexOf(userInput);
+  const computerRPSIndex = rpsArray.indexOf(computerInput);
+  if (userInput === computerInput) {
+    resultArray.push('Draw');
+  } else if (userRPSIndex - 1 === computerRPSIndex || userRPSIndex + 2 === computerRPSIndex) {
+    resultArray.push('Win');
+  } else {
+    resultArray.push('Lose');
+  }
+  console.log(resultArray);
+  return resultArray;
+}
+
+const drawResults = (resultInfo) => {
+  const formatResult = resultInfo[2] === 'Draw' ? "It's a draw!" : `You ${resultInfo[2]}!`
+
+  const resultDisplay = document.querySelector('.results p');
+  resultDisplay.textContent = `The threw ${resultInfo[0]} ... the computer throws ${resultInfo[1]}. ${formatResult}`;
+}
+
+const trackScore = (result) => {
+  const scoreDisplay = document.querySelector('.results h3');
+  if (result === 'Win') userScore++;
+  if (result === 'Lose') computerScore++;
+  scoreDisplay.textContent = `The score is You: ${userScore} - Computer: ${computerScore}`;
+}
 
 const buttons = document.querySelectorAll('button');
-const getUserInput = (event) => {
+
+const clickHandler = (event) => {
   console.log(event.target.className);
-  return event.target.className;
+  const userInput = event.target.className;
+  const computerInput = computerPlay();
+  const result = playRound(userInput, computerInput);
+  drawResults(result);
+  trackScore(result[2]);
 }
 
-buttons.forEach(button => button.addEventListener('click', getUserInput));
-
-
-// Function to play a single round of rock, paper, scissors
-const playRound = (userChoice, computerChoice) => {
-  // Switch to compare userChoice with computerChoice and return the result;
-  switch (userChoice) {
-    case computerChoice:
-      return 'Tie Game';
-    case 'rock':
-      return computerChoice === 'paper' ? 'Computer Wins' : 'User Wins';
-    case 'paper':
-      return computerChoice === 'scissors' ? 'Computer Wins' : 'User Wins';
-    case 'scissors':
-      return computerChoice === 'rock' ? 'Computer Wins' : 'User Wins';
-      // default to catch any errors.
-    default:
-      return 'Error';
-  }
-}
-
-// Plays a full RPS Game in best of 5 --> so first to 3 wins is the overall winner.
-const game = () => {
-  let computerScore = 0;
-  let userScore = 0;
-  let userInput, computerInput;
-  while (computerScore < 3 && userScore < 3) {
-    userInput = getUserInput();
-    computerInput = computerPlay();
-    let result = playRound(userInput, computerInput);
-    // check & display results/gamestate
-    if (result === 'Computer Wins') {
-      computerScore++;
-      console.log(`Computer wins this round! The current score is computer: ${computerScore} you: ${userScore}.`);
-    } else if (result === 'User Wins') {
-      userScore++;
-      console.log(`User wins this round! The current score is computer: ${computerScore} you: ${userScore}.`);
-    } else if (result === 'Tie Game') {
-      console.log(`It's a draw! The current score is computer: ${computerScore} you: ${userScore}.`);
-    } else {
-      console.log('Error');
-    }
-  }
-  // returns winner
-  if (computerScore > userScore) {
-    return `Computer wins by a score of ${computerScore} to ${userScore}!`;
-  }
-  if (userScore > computerScore) {
-    return `You win by a score of ${userScore} to ${computerScore}`;
-  }
-  return 'Error';
-}
+buttons.forEach(button => button.addEventListener('click', clickHandler));
