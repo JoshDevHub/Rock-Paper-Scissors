507f00d9617dc3f5efd3d86f8cbd0d4c7b041e70
jmsmith1018 <jmsmith1018@gmail.com>
2021-10-11 09:53:25 -0600

    Refactor and rename for improved readability/maintenance

--- main.js
+++ main.js
@@ -6,36 +6,38 @@ const getComputerInput = () =>
   rpsArray[Math.floor(Math.random() * rpsArray.length)];
 
 const playRound = (userInput, computerInput) => {
-  const resultArray = [userInput, computerInput];
+  let result = '';
 
+ /**
+  * Using index arithmetic with the rpsArray to check the winner saves a lot of 
+  * lines & avoids messy, nested conditional checking.
+  */ 
   const userRPSIndex = rpsArray.indexOf(userInput);
   const computerRPSIndex = rpsArray.indexOf(computerInput);
   if (userInput === computerInput) {
-    resultArray.push('Draw');
+    result += 'Draw';
   } else if (
     userRPSIndex - 1 === computerRPSIndex ||
     userRPSIndex + 2 === computerRPSIndex
   ) {
-    resultArray.push('Win');
+    result += 'Win';
+    userScore++;
   } else {
-    resultArray.push('Lose');
+    result += 'Lose';
+    computerScore++;
   }
-  console.log(resultArray);
-  return resultArray;
+  return result;
 };
 
-const drawResults = (resultInfo) => {
-  const formatResult =
-    resultInfo[2] === 'Draw' ? "It's a draw!" : `You ${resultInfo[2]}!`;
+const drawResults = (userInput, computerInput, result) => {
+  const formatResult = result === 'Draw' ? "It's a draw!" : `You ${result}!`;
 
   const resultDisplay = document.querySelector('.round-outcome');
-  resultDisplay.textContent = `You threw ${resultInfo[0]} ... the computer throws ${resultInfo[1]}. ${formatResult}`;
+  resultDisplay.textContent = `You threw ${userInput} ... the computer throws ${computerInput}. ${formatResult}`;
 };
 
-const trackScore = (result) => {
+const drawScore = () => {
   const scoreDisplay = document.querySelector('.total-score');
-  if (result === 'Win') userScore++;
-  if (result === 'Lose') computerScore++;
   scoreDisplay.textContent = `The score is You: ${userScore} - Computer: ${computerScore}`;
 };
 
@@ -55,29 +57,24 @@ const resetResultsBox = () => {
   resultDisplay.textContent = 'Click a button to Play!';
 };
 
-const resetGame = () => {
+const resetClickHandler = () => {
   computerScore = 0;
   userScore = 0;
 
   toggleModal();
-
   resetResultsBox();
 };
 
 const resetButton = document.querySelector('.modal-btn');
-resetButton.addEventListener('click', resetGame);
+resetButton.addEventListener('click', resetClickHandler);
 
 const rpsClickHandler = (event) => {
-  console.log(event.target.className);
   const userInput = event.target.getAttribute('data');
   const computerInput = getComputerInput();
   const result = playRound(userInput, computerInput);
-  drawResults(result);
-  trackScore(result[2]);
-  const gamestate = isGameOver();
-  if (gamestate) {
-    toggleModal();
-  }
+  drawResults(userInput, computerInput, result);
+  drawScore();
+  if (isGameOver()) toggleModal();
 };
 
 const rpsButtons = document.querySelectorAll('.rps-btn');
